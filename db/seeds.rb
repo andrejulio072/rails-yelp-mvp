cb902f0cf6d3763962e5a4dbcb6e2ae560fbb4cd
Andre Garcia <andrejulio072@hotmail.com>
2022-04-27 02:03:30 +0100

    coding

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ puts "Cleaning database..."
 Restaurant.destroy_all
 
 puts "Creating restaurants..."
-dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", stars: 5 }
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "07 59 78 66 26", category: "food", stars: 5 }
 pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", stars: 4 }
 
 [ dishoom, pizza_east ].each do |attributes|
@@ -22,6 +22,8 @@ puts "Finished!"
   Restaurant.create(
     name: Faker::Restaurant.name,
     address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: Faker::Restaurant.type,
     rating: rand(1..5)
   )
 end
