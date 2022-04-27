f0159b834941013f896ab879dd6d7657f65f231f
Andre Garcia <andrejulio072@hotmail.com>
2022-04-27 01:19:57 +0100

    coding

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
-  resources :restaurants
+
+  get "/restaurants", to: "restaurants#index"
+  get "/restaurants/:id", to: "restaurants#show"
+  # verb path, to: "control#action"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Create
+
+  # Read
+
+  #Update
+
+  #Delete
 end
