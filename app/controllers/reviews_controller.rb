2541f1638387bc679fe57031c1f0a4aa7b81ca3b
Andre Garcia <andrejulio072@hotmail.com>
2022-05-02 19:22:26 +0100

    coding

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,10 @@
 class ReviewsController < ApplicationController
+  def new
+    # we need @restaurant in our `simple_form_for`
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
